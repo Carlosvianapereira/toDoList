7be1e76486e864262e0fc4226426d0989b34d570
CARLOS ALBERLANIO VIANA PEREIRA <carlospereira6662@digitalcollege.internal>
2025-05-06 20:49:40 -0300

    toDo list

---
+++ script.js
@@ -0,0 +1,68 @@
+// const form = document.getElementById("todo-form");
+// const input = document.getElementById("todo-input");
+// const list = document.getElementById("todo-list");
+
+// form.addEventListener("submit", function (e){
+//     e.preventDefault();
+
+//     const tasktext = input.value.trim ();
+//     if (tasktext ==="") return;
+
+//     const li = document.createElement("li");
+
+//     const checkbox = document.createElement("input");
+//     checkbox.type = "checkbox";
+//     checkbox.addEventListener("change" , () => {
+//         li.classList.toggle("done" , checkbox.checked)
+//     });
+
+//     const span = document.createElement("span");
+//     span.textContent = tasktext;
+//     span.style.flex = "1"
+
+//     const deleteBtn = document.createElement("button");
+//     deleteBtn.textContent ="❌";
+//     deleteBtn.addEventListener("click", () => li.remove());
+
+//     li.appendChild(checkbox);
+//     li.appendChild(span);
+//     li.appendChild(deleteBtn);
+
+//     list.appendChild(li);
+//     input.value = "";
+
+// });
+
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const taskText = input.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.addEventListener("change", () => {
+    li.classList.toggle("done", checkbox.checked);
+  });
+
+  const span = document.createElement("span");
+  span.textContent = taskText;
+  span.style.flex = "1";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "❌";
+  deleteBtn.addEventListener("click", () => li.remove());
+
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+
+  list.appendChild(li);
+  input.value = "";
+});
